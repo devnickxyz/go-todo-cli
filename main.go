1ce961095335e2ab15357c650ac2cb84ee56eebd
devnickxyz <devnickxyz@gmail.com>
2025-04-06 22:09:31 +0200

    fix(pgx): add configs to pgx pool connection

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/devnickxyz/gotodo/cmd"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -32,20 +33,35 @@ func DB() (*pgxpool.Pool, error) {
 	db.Password = os.Getenv("PSQL_PASSWORD")
 	connDB := fmt.Sprintf("postgresql://%s:%s@%s", db.User, db.Password, db.URL)
 
-	pool, err = pgxpool.New(ctx, connDB)
+	config, err := pgxpool.ParseConfig(connDB)
+
+	// Set pool configuration
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.MaxConnLifetime = time.Hour
+	config.MaxConnIdleTime = 30 * time.Minute
+
+	// pool, err = pgxpool.New(ctx, connDB)
+	pool, err = pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
-		log.Fatal("Unable to connect to database", err)
+		log.Fatal("Unable to connect to database aka creating conection pool %w\n", err)
 	}
 	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
 		log.Fatal("Unable to ping db:", err)
 	}
 	fmt.Println("Connected to DB")
 
-	return pool, err
+	return pool, nil
 }
 
 func main() {
-	DB()
+	var err error
+	pool, err = DB()
+	if err != nil {
+		pool.Close()
+		// fmt.Errorf("%w\n", err)
+	}
 	cmd.Ctx = ctx
 	cmd.Pool = pool
 	cmd.Execute()
